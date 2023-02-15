1d54625dac0ab991f57b58ecedfceca3a3e532e0
xiange <xiangetg@msn.cn>
2023-02-15 16:54:35 +0800

    Update snap

--- packages/chili/src/snap/snapper.ts
+++ packages/chili/src/snap/snapper.ts
@@ -3,6 +3,7 @@
 import { CancellationToken, CursorType, I18n, IDocument, PubSub } from "chili-core";
 
 import {
+    SnapEventHandler,
     SnapLengthAtAxisData,
     SnapLengthAtAxisHandler,
     SnapLengthAtPlaneData,
@@ -10,31 +11,35 @@ import {
     SnapPointData,
     SnapPointEventHandler,
 } from "./snapEventHandler";
-import { SnapEventHandler } from "./snapEventHandler/snapEventHandler";
 import { SnapedData } from "./interfaces";
 
 export abstract class Snapper {
-    protected eventHandler?: SnapEventHandler;
-    protected cancellationToken: CancellationToken;
+    protected readonly eventHandler: SnapEventHandler;
+    protected readonly cancellationToken: CancellationToken;
 
     constructor() {
         this.cancellationToken = new CancellationToken();
+        this.eventHandler = this.getEventHandler();
     }
 
     protected abstract getEventHandler(): SnapEventHandler;
 
     async snap(document: IDocument, tip: keyof I18n): Promise<SnapedData | undefined> {
-        if (this.eventHandler === undefined) this.eventHandler = this.getEventHandler();
         document.viewer.setCursor(CursorType.Drawing);
         PubSub.default.pub("statusBarTip", tip);
+        await this.waitEventHandlerFinished(document);
+        document.viewer.setCursor(CursorType.Default);
+        PubSub.default.pub("clearStatusBarTip");
+        return this.eventHandler.snaped;
+    }
+
+    protected async waitEventHandlerFinished(document: IDocument) {
+        let handler = document.visualization.eventHandler;
         document.visualization.eventHandler = this.eventHandler;
         while (!this.cancellationToken.isCanceled) {
             await new Promise((r) => setTimeout(r, 30));
         }
-        document.visualization.clearEventHandler();
-        document.viewer.setCursor(CursorType.Default);
-        PubSub.default.pub("clearStatusBarTip");
-        return this.eventHandler.snaped;
+        document.visualization.eventHandler = handler;
     }
 }
 
