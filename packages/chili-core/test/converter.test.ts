831335aadb0a0132da3a33f05fe34231d0683202
xiange <xiangetg@msn.cn>
2023-04-29 21:18:49 +0800

    Fixed converter.test

--- packages/chili-core/test/converter.test.ts
+++ packages/chili-core/test/converter.test.ts
@@ -3,40 +3,40 @@
 import "reflect-metadata";
 
 import { XYZ } from "../src";
-import { ConverterBase, NumberConverter, StringConverter, XYZConverter } from "../src/converter";
+import { IConverter, NumberConverter, StringConverter, XYZConverter } from "../src/converter";
 
 describe("converter test", () => {
     test("test type", () => {
         let converters = [new XYZConverter(), new NumberConverter()];
-        expect(converters.every((x) => x instanceof ConverterBase)).toBeTruthy();
+        expect(converters.every((x) => (x as IConverter).convert !== undefined)).toBeTruthy();
     });
 
     test("test XYZConverter", () => {
         let converter = new XYZConverter();
         let xyz = XYZ.unitX;
         let c = converter.convert(xyz);
-        expect(c).toStrictEqual("1,0,0");
-        expect(converter.convertBack(c!)?.x).toBe(1);
-        expect(converter.convertBack("1")).toBeUndefined();
-        expect(converter.convertBack("1, 1, 1, 1")).toBeUndefined();
-        expect(converter.convertBack("1, 1, 1")).toStrictEqual(new XYZ(1, 1, 1));
+        expect(c.value).toStrictEqual("1,0,0");
+        expect(converter.convertBack(c.value!).value?.x).toBe(1);
+        expect(converter.convertBack("1").value).toBeUndefined();
+        expect(converter.convertBack("1, 1, 1, 1").value).toBeUndefined();
+        expect(converter.convertBack("1, 1, 1").value).toStrictEqual(new XYZ(1, 1, 1));
     });
 
     test("test NumberConverter", () => {
         let converter = new NumberConverter();
-        expect(converter.convert(Number.NaN)).toBeUndefined();
-        expect(converter.convert(-20)).toBe("-20");
-        expect(converter.convert(20)).toBe("20");
-        expect(converter.convert(1e3)).toBe("1000");
-        expect(converter.convertBack("NaN")).toBeUndefined();
-        expect(converter.convertBack("1a")).toBeUndefined();
-        expect(converter.convertBack("1E-3")).toBe(0.001);
-        expect(converter.convertBack("-3")).toBe(-3);
+        expect(converter.convert(Number.NaN).value).toBeUndefined();
+        expect(converter.convert(-20).value).toBe("-20");
+        expect(converter.convert(20).value).toBe("20");
+        expect(converter.convert(1e3).value).toBe("1000");
+        expect(converter.convertBack("NaN").value).toBeUndefined();
+        expect(converter.convertBack("1a").value).toBeUndefined();
+        expect(converter.convertBack("1E-3").value).toBe(0.001);
+        expect(converter.convertBack("-3").value).toBe(-3);
     });
 
     test("test StringConverter", () => {
         let converter = new StringConverter();
-        expect(converter.convert("")).toBe("");
-        expect(converter.convertBack("")).toBe("");
+        expect(converter.convert("").value).toBe("");
+        expect(converter.convertBack("").value).toBe("");
     });
 });
