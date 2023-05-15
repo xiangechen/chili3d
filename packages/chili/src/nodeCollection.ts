b5eff63980011dd6221b38766f62e43a95331117
xiange <xiangetg@msn.cn>
2023-05-15 16:14:39 +0800

    Fixed add bug

--- packages/chili/src/nodeCollection.ts
+++ packages/chili/src/nodeCollection.ts
@@ -21,27 +21,40 @@ export class NodeCollection implements INodeCollection {
     }
 
     add(...nodes: INode[]) {
-        let items = this.getItems(nodes, (node) => this.nodeMap.set(node.id, node));
+        let items = this.addNodes(nodes);
         if (items.length === 0) return;
         this.document.visual.context.addModel(nodes.filter((node) => INode.isModelNode(node)) as IModel[]);
         PubSub.default.pub("nodeAdded", this.document, items);
         Transaction.add(this.document, new CollectionHistoryRecord(this, CollectionAction.add, items));
     }
 
+    private addNodes(nodes: INode[]) {
+        let items = new Array(nodes.length);
+        let index = 0;
+        for (const node of nodes) {
+            if (!this.nodeMap.has(node.id)) {
+                this.nodeMap.set(node.id, node);
+                items[index++] = node;
+            }
+        }
+        items.splice(index);
+        return items;
+    }
+
     remove(...nodes: INode[]) {
-        let items = this.getItems(nodes, (node) => this.nodeMap.delete(node.id));
+        let items = this.removeNodes(nodes);
         if (items.length === 0) return;
         this.document.visual.context.removeModel(nodes.filter((node) => INode.isModelNode(node)) as IModel[]);
         PubSub.default.pub("nodeRemoved", this.document, items);
         Transaction.add(this.document, new CollectionHistoryRecord(this, CollectionAction.remove, items));
     }
 
-    private getItems(nodes: INode[], handleNodeAction: (node: INode) => void) {
-        let items = new Array(nodes.length);
+    private removeNodes(nodes: INode[]) {
+        let items: INode[] = new Array(nodes.length);
         let index = 0;
         for (const node of nodes) {
             if (this.nodeMap.has(node.id)) {
-                handleNodeAction(node);
+                this.nodeMap.delete(node.id);
                 items[index++] = node;
             }
         }
