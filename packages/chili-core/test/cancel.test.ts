a52eb91bc9e2fd12ee25044b2ef3e4d1092b3d62
xiange <xiangetg@msn.cn>
2023-06-05 21:13:27 +0800

    Fixed test

--- packages/chili-core/test/cancel.test.ts
+++ packages/chili-core/test/cancel.test.ts
@@ -6,12 +6,12 @@ import { TaskToken } from "../src";
 
 test("test CancellationToken", async () => {
     let token = new TaskToken();
-    expect(token.isCanceled).toBeFalsy();
+    expect(token.isCancelled).toBeFalsy();
     await new Promise((r, s) => {
         setTimeout(() => {
             token.cancel();
             r("resolved");
         }, 30);
     });
-    expect(token.isCanceled).toBeTruthy();
+    expect(token.isCancelled).toBeTruthy();
 });
