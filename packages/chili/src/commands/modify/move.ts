f560245f2bc7339c255bc19b34fe9a27968f9a7d
xiange <xiangetg@msn.cn>
2023-06-10 17:42:21 +0800

    ✨ feat(chili): add a preview of the moving process

--- packages/chili/src/commands/modify/move.ts
+++ packages/chili/src/commands/modify/move.ts
@@ -1,9 +1,19 @@
 // Copyright 2022-2023 the Chili authors. All rights reserved. MPL-2.0 license.
 
-import { IDocument, IModel, INode, IShape, ShapeMeshData, XYZ, command } from "chili-core";
-import { MultistepCommand } from "../multistepCommand";
-import { IStep, PointStep } from "../../step";
+import {
+    Config,
+    EdgeMeshData,
+    IDocument,
+    IModel,
+    INode,
+    LineType,
+    Transaction,
+    XYZ,
+    command,
+} from "chili-core";
 import { Dimension, SnapPointData } from "../../snap";
+import { IStep, PointStep } from "../../step";
+import { MultistepCommand } from "../multistepCommand";
 
 @command({
     name: "Move",
@@ -12,6 +22,7 @@ import { Dimension, SnapPointData } from "../../snap";
 })
 export class Move extends MultistepCommand {
     private models?: IModel[];
+    private positions?: number[];
 
     getSteps(): IStep[] {
         let firstStep = new PointStep("operate.pickFistPoint");
@@ -27,18 +38,41 @@ export class Move extends MultistepCommand {
         };
     };
 
-    private movePreview = (point: XYZ): ShapeMeshData | undefined => {
-        return;
+    private movePreview = (point: XYZ): EdgeMeshData | undefined => {
+        let start = this.stepDatas[0].point;
+        let positions = [...this.positions!];
+        let { x, y, z } = point.sub(start);
+        for (let i = 0; i < this.positions!.length; i++) {
+            if (i % 3 === 0) positions[i] += x;
+            else if (i % 3 === 1) positions[i] += y;
+            else if (i % 3 === 2) positions[i] += z;
+        }
+        positions.push(start.x, start.y, start.z, point.x, point.y, point.z);
+        return {
+            positions,
+            lineType: LineType.Solid,
+            color: Config.instance.visual.temporaryEdgeColor,
+            groups: [],
+        };
     };
 
     protected override beforeExcute(document: IDocument): Promise<boolean> {
         this.models = document.selection.getSelectedNodes().filter((x) => INode.isModelNode(x)) as IModel[];
+        this.positions = [];
+        this.models?.forEach((model) => {
+            let ps = model.shape()?.mesh().edges?.positions;
+            if (!ps) return;
+            this.positions = this.positions!.concat(ps);
+        });
         return Promise.resolve(this.models.length > 0);
     }
 
     protected excuting(document: IDocument): void {
-        this.models?.forEach((x) => {
-            x.translation = this.stepDatas[1].point.sub(this.stepDatas[0].point);
+        Transaction.excute(document, `excute ${Object.getPrototypeOf(this).data.name}`, () => {
+            this.models?.forEach((x) => {
+                x.translation = this.stepDatas[1].point.sub(this.stepDatas[0].point);
+            });
+            document.visual.viewer.redraw();
         });
     }
 }
