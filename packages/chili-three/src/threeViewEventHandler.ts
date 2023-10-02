ef0b197f3ff40b5277f1e81d4ca1c81fbffbb64b
xiange <xiangetg@msn.cn>
2023-10-03 00:10:19 +0800

    🐞 fix: Fix the bug in ThreeViewHandler for the middle mouse button

--- packages/chili-three/src/threeViewEventHandler.ts
+++ packages/chili-three/src/threeViewEventHandler.ts
@@ -3,81 +3,67 @@
 import { IEventHandler, IView } from "chili-core";
 
 interface MouseDownData {
-    x: number;
-    y: number;
     time: number;
     key: number;
-    isDoubleClick: boolean;
 }
 
 const MIDDLE = 4;
 
 export class ThreeViewHandler implements IEventHandler {
-    private lastDown: MouseDownData | undefined;
+    private _lastDown: MouseDownData | undefined;
     private _clearDownId: any;
 
     dispose() {}
 
     mouseWheel(view: IView, event: WheelEvent): void {
-        view.zoom(event.offsetX, event.offsetY, event.deltaY);
+        view.cameraController.zoom(event.offsetX, event.offsetY, event.deltaY);
         view.update();
     }
 
     pointerMove(view: IView, event: PointerEvent): void {
-        if (this.lastDown && event.buttons === MIDDLE) {
-            let dx = this.lastDown.x - event.offsetX;
-            let dy = event.offsetY - this.lastDown.y;
+        if (event.buttons === MIDDLE) {
             if (event.shiftKey) {
-                view.pan(dx, dy);
+                view.cameraController.pan(-event.movementX, event.movementY);
             } else if (!event.shiftKey) {
-                view.rotate(dx, dy);
+                view.cameraController.rotate(-event.movementX, -event.movementY);
             }
-            this.lastDown.x = event.offsetX;
-            this.lastDown.y = event.offsetY;
+            if (event.movementX !== 0 && event.movementY !== 0) this._lastDown = undefined;
+            view.update();
         }
-        view.update();
     }
 
     pointerDown(view: IView, event: PointerEvent): void {
+        this.clearTimeout();
+        if (this._lastDown && this._lastDown.time + 500 > Date.now() && event.buttons === MIDDLE) {
+            this._lastDown = undefined;
+            view.cameraController.fitContent();
+            view.update();
+        } else if (event.buttons === MIDDLE) {
+            this._lastDown = {
+                time: Date.now(),
+                key: event.buttons,
+            };
+        }
+    }
+
+    private clearTimeout() {
         if (this._clearDownId) {
             clearTimeout(this._clearDownId);
             this._clearDownId = undefined;
         }
-        let isDoubleClick = false;
-        if (this.checkDoubleClick(event)) {
-            isDoubleClick = true;
-            view.fitContent();
-            view.update();
-        }
-        this.lastDown = {
-            x: event.offsetX,
-            y: event.offsetY,
-            time: Date.now(),
-            key: event.buttons,
-            isDoubleClick,
-        };
     }
 
     pointerOut(view: IView, event: PointerEvent): void {
-        this.lastDown = undefined;
-    }
-
-    private checkDoubleClick(event: PointerEvent) {
-        return (
-            this.lastDown &&
-            !this.lastDown.isDoubleClick &&
-            this.lastDown.key === event.buttons &&
-            this.lastDown.time + 500 > Date.now() &&
-            event.buttons === MIDDLE &&
-            !event.shiftKey
-        );
+        this._lastDown = undefined;
     }
 
     pointerUp(view: IView, event: PointerEvent): void {
-        this._clearDownId = setTimeout(() => {
-            this.lastDown = undefined;
-            this._clearDownId = undefined;
-        }, 500);
+        if (event.buttons === MIDDLE && this._lastDown) {
+            this._clearDownId = setTimeout(() => {
+                this._lastDown = undefined;
+                this._clearDownId = undefined;
+            }, 500);
+        }
     }
 
     keyDown(view: IView, event: KeyboardEvent): void {}
