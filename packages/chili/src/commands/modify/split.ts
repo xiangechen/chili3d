9c97cc3ca5917dd5f37e716569fddd1af616100b
xiange <xiangetg@msn.cn>
2024-05-29 19:38:09 +0800

    🦄 refactor: remove tool shapes

--- packages/chili/src/commands/modify/split.ts
+++ packages/chili/src/commands/modify/split.ts
@@ -1,6 +1,15 @@
 // Copyright 2022-2023 the Chili authors. All rights reserved. AGPL-3.0 license.
 
-import { EditableGeometryEntity, GeometryEntity, IEdge, ShapeType, command } from "chili-core";
+import {
+    EditableGeometryEntity,
+    GeometryEntity,
+    GeometryModel,
+    IEdge,
+    IVisualGeometry,
+    ShapeType,
+    Transaction,
+    command,
+} from "chili-core";
 import { IStep } from "../../step";
 import { SelectShapeStep } from "../../step/selectStep";
 import { CreateCommand } from "../createCommand";
@@ -18,10 +27,37 @@ export class Split extends CreateCommand {
         return new EditableGeometryEntity(this.document, shapes);
     }
 
+    protected override executeMainTask() {
+        Transaction.excute(this.document, `excute ${Object.getPrototypeOf(this).data.name}`, () => {
+            let geometry = this.geometryEntity() as EditableGeometryEntity;
+            let old = this.document.visual.context.getModel(this.stepDatas[0].shapes[0].owner)!;
+            if (old instanceof EditableGeometryEntity) {
+                old.replaceShape(geometry.shape.unwrap());
+            } else {
+                let model = new GeometryModel(this.document, old.name, geometry);
+                model.geometry.matrix = old.geometry.matrix;
+                this.removeModels(
+                    this.stepDatas[0].shapes[0].owner,
+                    ...this.stepDatas[1].shapes.map((x) => x.owner),
+                );
+                this.document.addNode(model);
+            }
+            this.document.visual.highlighter.clear();
+            this.document.visual.update();
+        });
+    }
+
+    private removeModels(...shapes: IVisualGeometry[]) {
+        shapes.forEach((x) => {
+            let model = this.document.visual.context.getModel(x);
+            model?.parent?.remove(model);
+        });
+    }
+
     protected override getSteps(): IStep[] {
         return [
             new SelectShapeStep(ShapeType.Shape, "prompt.select.shape", false),
-            new SelectShapeStep(ShapeType.Edge, "prompt.select.shape", true),
+            new SelectShapeStep(ShapeType.Edge | ShapeType.Wire, "prompt.select.shape", true),
         ];
     }
 }
