7d95bad33d6f3dbbec73ff7b010b5f3fe0f04ab7
xiange <xiangetg@msn.cn>
2025-03-31 21:18:06 +0800

    🦄 refactor: delayed generation mesher

--- packages/chili-wasm/src/mesher.ts
+++ packages/chili-wasm/src/mesher.ts
@@ -9,16 +9,11 @@ import {
     ShapeMeshGroup,
     VisualConfig,
 } from "chili-core";
-import {
-    Mesher as OccMesher,
-    EdgeMeshData as OccEdgeMeshData,
-    FaceMeshData as OccFaceMeshData,
-} from "../lib/chili-wasm";
+import { EdgeMeshData as OccEdgeMeshData, FaceMeshData as OccFaceMeshData } from "../lib/chili-wasm";
 import { OcctHelper } from "./helper";
 import { OccShape } from "./shape";
 
 export class Mesher implements IShapeMeshData, IDisposable {
-    private _occMesher: OccMesher;
     private _isMeshed = false;
     private _lines?: EdgeMeshData;
     private _faces?: FaceMeshData;
@@ -36,9 +31,7 @@ export class Mesher implements IShapeMeshData, IDisposable {
         return this._faces;
     }
 
-    constructor(private shape: OccShape) {
-        this._occMesher = new wasm.Mesher(shape.shape, 0.005);
-    }
+    constructor(private shape: OccShape) {}
 
     private mesh() {
         if (this._isMeshed) {
@@ -47,7 +40,8 @@ export class Mesher implements IShapeMeshData, IDisposable {
         this._isMeshed = true;
 
         gc((c) => {
-            const meshData = c(this._occMesher.mesh());
+            const occMesher = c(new wasm.Mesher(this.shape.shape, 0.005));
+            const meshData = c(occMesher.mesh());
             const faceMeshData = c(meshData.faceMeshData);
             const edgeMeshData = c(meshData.edgeMeshData);
 
@@ -84,8 +78,6 @@ export class Mesher implements IShapeMeshData, IDisposable {
         this.shape = null as any;
         this._faces = null as any;
         this._lines = null as any;
-        this._occMesher.delete();
-        this._occMesher = null as any;
     }
 
     updateMeshShape(): void {
