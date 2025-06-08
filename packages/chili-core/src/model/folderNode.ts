1aabbdccdeb5d6c3933fd3fbaf6781ef6a10c73f
xiange <xiangetg@msn.cn>
2025-06-08 23:28:22 +0800

    🐞 fix: fixed visible bug

--- packages/chili-core/src/model/folderNode.ts
+++ packages/chili-core/src/model/folderNode.ts
@@ -51,6 +51,7 @@ export class FolderNode extends Node implements INodeLinkedList {
 
     private initNode(node: INode): boolean {
         node.parent = this;
+        node.parentVisible = this.visible && this.parentVisible;
         if (!this._firstChild) {
             this._firstChild = this._lastChild = node;
             node.previousSibling = node.nextSibling = undefined;
@@ -119,6 +120,7 @@ export class FolderNode extends Node implements INodeLinkedList {
     private removeNode(node: INode, nullifyParent: boolean) {
         if (nullifyParent) {
             node.parent = undefined;
+            node.parentVisible = true;
         }
 
         if (node === this._firstChild) {
