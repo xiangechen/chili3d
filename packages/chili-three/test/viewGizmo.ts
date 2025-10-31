46b95620db8fb031b3d68e52c4de842a5e4ec152
xiange <xiangetg@msn.cn>
2025-10-31 14:27:33 +0800

    ✨ feat: implement IViewGizmo interface in moduleNameMapper class

--- packages/chili-three/test/viewGizmo.ts
+++ packages/chili-three/test/viewGizmo.ts
@@ -1,6 +1,12 @@
 // Part of the Chili3d Project, under the AGPL-3.0 License.
 // See LICENSE file in the project root for full license information.
 
-export class ViewGizmo extends HTMLElement {}
+import { IViewGizmo } from "chili-core";
+
+export class ViewGizmo extends HTMLElement implements IViewGizmo {
+    update(): void {}
+    dispose(): void {}
+    setDom(dom: HTMLElement) {}
+}
 
 customElements.define("view-gizmo", ViewGizmo);
