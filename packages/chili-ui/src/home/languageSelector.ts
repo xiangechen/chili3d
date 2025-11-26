a9c77f2fe934bff7ea26aba016772be9f2c1e247
xiange <xiangetg@msn.cn>
2025-11-26 09:36:05 +0800

    ✨ feat: add chili-i18n

--- packages/chili-ui/src/home/languageSelector.ts
+++ packages/chili-ui/src/home/languageSelector.ts
@@ -6,19 +6,19 @@ import { Config, I18n } from "chili-core";
 
 export const LanguageSelector = (props: HTMLProps<HTMLElement>) => {
     const languages: HTMLOptionElement[] = [];
-    I18n.languages.forEach((language) =>
+    I18n.getLanguages().forEach((language) => {
         languages.push(
             option({
-                selected: language.code === I18n.currentLanguage(),
+                selected: language.language === I18n.currentLanguage(),
                 textContent: language.display,
             }),
-        ),
-    );
+        );
+    });
     return select(
         {
             onchange: (e) => {
                 const language = (e.target as HTMLSelectElement).selectedIndex;
-                Config.instance.language = Array.from(I18n.languages.keys())[language];
+                Config.instance.language = I18n.getLanguages()[language].language;
             },
             ...props,
         },
